1c849188f3fc546e676aac7ec343c2b81f444e55
Jasmine Kaur <jasminekaur@Jasmines-Air-3.lan>
2026-04-08 23:35:19 -0500

    added config

---
+++ src/config.py
@@ -0,0 +1,21 @@
+BATCH_SIZE = 4
+
+# Time (in milliseconds) to wait before processing a batch
+BATCH_TIMEOUT_MS = 100
+
+
+# Time-to-live for cache entries (in seconds)
+CACHE_TTL_SECONDS = 300  # 5 minutes
+
+# Maximum number of entries allowed in cache
+CACHE_MAX_ENTRIES = 100
+
+
+MODEL_NAME = "distilgpt2"
+
+# Max tokens to generate per request
+MAX_NEW_TOKENS = 30
+
+
+HOST = "127.0.0.1"
+PORT = 8000
